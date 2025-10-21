f339f055f6acf81aef8d621e12046734d195f582
ehasun15019 <ehasunulislam0202@gmail.com>
2025-10-22 00:03:24 +0600

    complete footer

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,29 +1,58 @@
 import React from "react";
+import { assets } from "../../assets/assets";
+import { Link } from "react-router";
 
 const Footer = () => {
   return (
-    <footer className="footer sm:footer-horizontal bg-neutral text-neutral-content p-10">
-      <nav>
-        <h6 className="footer-title">Services</h6>
-        <a className="link link-hover">Branding</a>
-        <a className="link link-hover">Design</a>
-        <a className="link link-hover">Marketing</a>
-        <a className="link link-hover">Advertisement</a>
-      </nav>
-      <nav>
-        <h6 className="footer-title">Company</h6>
-        <a className="link link-hover">About us</a>
-        <a className="link link-hover">Contact</a>
-        <a className="link link-hover">Jobs</a>
-        <a className="link link-hover">Press kit</a>
-      </nav>
-      <nav>
-        <h6 className="footer-title">Legal</h6>
-        <a className="link link-hover">Terms of use</a>
-        <a className="link link-hover">Privacy policy</a>
-        <a className="link link-hover">Cookie policy</a>
-      </nav>
-    </footer>
+   <footer style={{ backgroundImage: `url(${assets.footer})` }} className="flex flex-col md:flex-row gap-9 h-[550px] py-5 md:py-0 items-center w-full">
+    <section className="ps-0 md:ps-20">
+      <Link to="/">
+        <img src={assets.logo} alt="logo" className="w-40 mb-2" />
+      </Link>
+
+      <p>Nunc consequat interdum varius <br /> sit amet mattis. </p>
+
+      <div className="social-icon flex gap-4 mt-3">
+        <Link to="/">
+          <img src={assets.s1} alt="" className="w-8" />
+        </Link>      
+
+        <Link to="/">
+        <img src={assets.s2} alt="" className="w-8" />
+        </Link>
+
+        <Link to="/">
+        <img src={assets.s3} alt="" className="w-8" />
+        </Link>
+
+        <Link to="/">
+        <img src={assets.s4} alt="" className="w-8" />
+        </Link>
+      </div>
+    </section>
+
+    <section>
+      <h4 className="text-[1.2rem] pb-3">My account</h4>
+
+      <ul>
+        <li><Link to="/">Track my order</Link></li>
+        <li><Link to="/">Terms of use</Link></li>
+        <li><Link to="/">Wishlist</Link></li>
+        <li><Link to="/">Submit Your feedback</Link></li>
+      </ul>
+    </section>
+
+    <section>
+      <h4 className="text-[1.2rem] pb-3">Customer service</h4>
+
+      <ul>
+        <li><Link to="/">Monday to Friday</Link></li>
+        <li><Link to="/">10am - 6pm( NewYork time)</Link></li>
+        <li>Call us: <span className="text-blue-500">123-456-7868</span></li>
+        <li>Email us: <span className="text-blue-500">info@example.com</span></li>
+      </ul>
+    </section>
+   </footer>
   );
 };
 
--- src/assets/assets.js
+++ src/assets/assets.js
@@ -3,6 +3,7 @@ import logo from "./Toytopia.webp"
 import slider1 from "./Slider1.png"
 import slider2 from "./Slider2.jpg"
 import slider3 from "./Slider3.jpg"
+import footer from "./Footer.png"
 
 // find product section 
 import f1 from "./f1.png"
@@ -12,11 +13,19 @@ import f4 from "./f4.png"
 import f5 from "./f5.png"
 
 
+// social icon section
+import s1 from "./s1.png"
+import s2 from "./s2.png"
+import s3 from "./s3.png"
+import s4 from "./s4.png"
+
 export const assets = {
     toolbar,
     logo,
     slider1, slider2, slider3,
-    f1,f2,f3,f4,f5
+    f1,f2,f3,f4,f5,
+    footer,
+    s1,s2, s3,s4
 }
 
 export const findToys = [
