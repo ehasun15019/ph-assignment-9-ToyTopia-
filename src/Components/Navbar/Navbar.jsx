9fd50c740151c4f26e7f1089161dc057888a0e20
ehasun15019 <ehasunulislam0202@gmail.com>
2025-10-21 21:50:30 +0600

    complete 70% navbar in Home page

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,77 +1,49 @@
-import React from "react";
+import { RiMenu2Line } from "react-icons/ri";
+import { assets } from "../../assets/assets";
+import { Link, NavLink } from "react-router";
+
+const navLink = (
+  <>
+    <NavLink to="/" className="me-3">
+      Home
+    </NavLink>
+    <NavLink to="/" className="me-3">
+      Blog
+    </NavLink>
+    <NavLink to="/" className="me-3">
+      Contact
+    </NavLink>
+    <NavLink to="/" className="me-3">
+      My Profile
+    </NavLink>
+  </>
+);
 
 const Navbar = () => {
   return (
-    <div className="navbar bg-base-100 shadow-sm mt-10">
+    <div className="navbar mt-10 px-3 md:px-8 lg:px-10">
       <div className="navbar-start">
-        <div className="dropdown">
+        <Link to="/" className="ext-xl">
+          <img src={assets.logo} alt="logo" className="w-30" />
+        </Link>
+      </div>
+      <div className="navbar-center hidden lg:flex">
+        <ul className="menu menu-horizontal px-1">{navLink}</ul>
+      </div>
+      <div className="navbar-end ">
+        <a className="btn bg-[#0F83B2] text-white rounded-full px-6">Login</a>
+
+        <div className="dropdown relative">
           <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              className="h-5 w-5"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-            >
-              {" "}
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                d="M4 6h16M4 12h8m-8 6h16"
-              />{" "}
-            </svg>
+            <RiMenu2Line />
           </div>
           <ul
             tabIndex="-1"
-            className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow"
+            className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow right-0"
           >
-            <li>
-              <a>Item 1</a>
-            </li>
-            <li>
-              <a>Parent</a>
-              <ul className="p-2">
-                <li>
-                  <a>Submenu 1</a>
-                </li>
-                <li>
-                  <a>Submenu 2</a>
-                </li>
-              </ul>
-            </li>
-            <li>
-              <a>Item 3</a>
-            </li>
+            {navLink}
           </ul>
         </div>
-        <a className="btn btn-ghost text-xl">daisyUI</a>
-      </div>
-      <div className="navbar-center hidden lg:flex">
-        <ul className="menu menu-horizontal px-1">
-          <li>
-            <a>Item 1</a>
-          </li>
-          <li>
-            <details>
-              <summary>Parent</summary>
-              <ul className="p-2">
-                <li>
-                  <a>Submenu 1</a>
-                </li>
-                <li>
-                  <a>Submenu 2</a>
-                </li>
-              </ul>
-            </details>
-          </li>
-          <li>
-            <a>Item 3</a>
-          </li>
-        </ul>
-      </div>
-      <div className="navbar-end">
-        <a className="btn">Button</a>
       </div>
     </div>
   );
--- src/assets/assets.js
+++ src/assets/assets.js
@@ -1,5 +1,8 @@
 import toolbar from "./Toolbar.png"
+import logo from "./Toytopia.webp"
+
 
 export const assets = {
-    toolbar
+    toolbar,
+    logo
 }
